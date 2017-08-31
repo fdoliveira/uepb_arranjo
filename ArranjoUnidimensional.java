a7fc8e5de185eb29afe8c69c79626d32490815b3
Francois Oliveira <francois@objetorelacional.com.br>
2017-08-31 00:04:46 -0300

    Add new examples and keynote presentation

--- ArranjoUnidimensional.java
+++ ArranjoUnidimensional.java
@@ -1,25 +1,21 @@
 public class ArranjoUnidimensional {
 
     public static void main(String[] args) {
+        // Declaração do Array
         int[] dados;
 
+        // Inicialização do Array
         dados = new int[10];
 
+        // Acessando do Array
         dados[0] = 3;
         dados[1] = 2;
         dados[2] = 1;
         dados[3] = 2;
         dados[4] = 6;
-        dados[5] = 7;
-        dados[6] = 9;
-        dados[7] = 10;
-        dados[8] = 3;
-        dados[9] = 22;
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("dados[" + i + "] = " + dados[i]);
-        }
-        
+        }        
     }
-
 }
--- OrdenarTresValores.java
+++ OrdenarTresValores.java
@@ -1,19 +1,45 @@
+import java.util.Scanner;
+
 public class OrdenarTresValores {
 
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
         
-        System.out.println("Entre com o valor de A: ");
+        System.out.print("Entre com o valor de A: ");
         int a = input.nextInt();
         
-        System.out.println("Entre com o valor de B: ");
+        System.out.print("Entre com o valor de B: ");
         int b = input.nextInt();
         
-        System.out.println("Entre com o valor de C: ");
+        System.out.print("Entre com o valor de C: ");
         int c = input.nextInt();
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        System.out.println("");
+        System.out.print("Os valores em ordem: ");
+
+        if (a > b) {
+            if (b > c) {
+                System.out.printf("%s, %s, %s", c, b, a);
+            } else {
+                if (a > c) {
+                    System.out.printf("%s, %s, %s", b, c, a);
+                } else {
+                    System.out.printf("%s, %s, %s", b, a, c);
+                }
+            }
+        } else {
+            if (a > c) {
+                System.out.printf("%s, %s, %s", c, a, b);
+            } else {
+                if (b > c) {
+                    System.out.printf("%s, %s, %s", a, c, b);
+                } else {
+                    System.out.printf("%s, %s, %s", a, b, c);
+                }
+            }            
+        }
+
+        System.out.println("");
+        System.out.println("FIM");
     }
 }
